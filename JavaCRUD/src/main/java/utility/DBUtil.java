c71a0f899278a7a386c86fa28f54e6e7811bffb9
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2021-05-02 12:33:21 +0530

    Update DBUtil.java

--- JavaCRUD/src/main/java/utility/DBUtil.java
+++ JavaCRUD/src/main/java/utility/DBUtil.java
@@ -7,19 +7,19 @@ import java.sql.SQLException;
 public class DBUtil {
 
 	private static final String url = "jdbc:mysql://localhost:3306/example"; // jdbc:mysql://localhost:3306/<YOUR_DB_NAME>
-    private static final String username = "root";	// DB user name, default it is "root" 
-    private static final String password = "";	// User password, default it is ""
-    private static Connection conn;
+	private static final String username = "root";	// DB user name, default it is "root" 
+   	private static final String password = "";	// User password, default it is ""
+   	private static Connection conn;
     
-    public static Connection getConnection() {
-    	try {
-    		Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection(url, username, password);
+    	public static Connection getConnection() {
+    		try {
+    			Class.forName("com.mysql.jdbc.Driver");
+            		conn = DriverManager.getConnection(url, username, password);
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
     	
-    	return conn;
-    }
+    		return conn;
+    	}
 	
 }
